bee1a1f20b34e68c8cae6d48041e08084e83ceaa
Eu-Loong Lim <euloong@hotmail.com>
2021-06-16 16:46:44 +1000

    added loop to process neighbouring nodes distances to node A

--- scratch.rb
+++ scratch.rb
@@ -1,12 +1,10 @@
 # Graph: AB5, BC4, CD8, DC8, DE6, AD5, CE2, EB3, AE7
-
 #      A   B   C   D   E
 # A    0   5   0   5   7
 # B    0   0   4   0   0
 # C    0   0   0   8   2
 # D    0   0   8   0   6
 # E    0   3   0   0   0
-
 adjacency_matrix = [
     [0, 5, 0, 5, 7],
     [0, 0, 4, 0, 0],
@@ -15,40 +13,53 @@ adjacency_matrix = [
     [0, 3, 0, 0, 0]
 ]
 
-#The length of the shortest route (in terms of distance to travel) from A to C. Answer: 9
-# Initial state:
-# Distance start vertex from start vertex = 0
-# Distance of all other vertices from start = infinity or a really large number like 100
-# Visited vertices = [ ]
-# Unvisited vertices = [A, B, C, D, E]
-
+#Find the length of the shortest route (in terms of distance to travel) from A to C. Answer: 9
+#initial state
+@infinity = 100
 visited_vertices = []
-unvisited_vertices = [0, 1, 2, 3, 4]
+unvisited_vertices = ['A', 'B', 'C', 'D', 'E']
+known_distance_from_A = [0, @infinity, @infinity, @infinity, @infinity]
+puts "Initial state is: #{known_distance_from_A}"
 
+start_node_value = unvisited_vertices.first
+puts "The start node is: #{start_node_value}"
+start_node_index = unvisited_vertices.index(start_node_value)
 # WHILE vertices remain unvisited
-        #while visited_vertices != unvisited_vertices
-        #end
+    #while unvisited_vertices.size > 0
+# 	Visit the unvisited vertex with the smallest known distance from the start vertex (the current vertex). This is A-A = 0
+visited_status_of_start_node = visited_vertices.include?(start_node_index)
+puts "Visit status of start node is #{visited_status_of_start_node}"
 
-# 	Visit the unvisited vertex with the smallest known distance from the start vertex (the current vertex)
-current_node = unvisited_vertices.first
-puts "Current node is #{current_node}"
-
-visited_status_of_current_node = visited_vertices.include?(current_node)
-puts "Visit status of current node is #{visited_status_of_current_node}"
-
-if (visited_status_of_current_node == false)
-    puts "Array of distance to neighbouring nodes from current node: #{adjacency_matrix[current_node]}"
-    #find the first smallest number > 0
-    #store it's value(distance) and index(node)
-    #loop through each value > 0
-end
-
+if (visited_status_of_start_node == false)
+    calculated_distance_from_A = adjacency_matrix[start_node_index]
+    puts "Calculated distances of neighbouring nodes from start node: #{calculated_distance_from_A}"
 # 	FOR each unvisited neighbour of the current vertex
-# 		calculate distance the distance from the start vertex
+# 		calculate the distance from the start vertex
 # 		IF the calculated distance is less than the known distance
 # 			Update shortest distance to this vertex
+    calculated_distance_from_A.each_with_index do |calculated_distance, index|
+        if calculated_distance < known_distance_from_A[index]
+            known_distance_from_A[index] = calculated_distance
+            if calculated_distance_from_A[index] < known_distance_from_A[index]
+                known_distance_from_A[index] = calculated_distance_from_A[index] + known_distance_from_A[index]
+            end
+        end
+    end
+    puts "New state is: #{known_distance_from_A}"
+end
 # Update the previous vertex with the current vertex
+#question: how do I log the previous vertex? do I want to?
 # end if
 # 	NEXT unvisited neighbour
 # 	Add the current vertex to the list of visited vertices
+visited_vertices << start_node_value
+#To do: need to overwrite the unvisted vertice matrix otherwise infinite loop
+new_unvisited_vertices = unvisited_vertices.drop(1)
+puts "visited vertices: #{visited_vertices}"
+puts "new unvisited vertices: #{new_unvisited_vertices}"
+new_start_node = new_unvisited_vertices.first
+puts "new start node: #{new_start_node}"
+start_node_index = new_unvisited_vertices.index(new_start_node)
+puts "start node index(should be 0): #{start_node_index}"
 # End WHILE
+#end
