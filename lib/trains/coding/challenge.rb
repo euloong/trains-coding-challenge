f7b9e9c51399ead66eb0882fa7080ab71b070168
Eu-Loong Lim <euloong@hotmail.com>
2021-06-21 14:45:48 +1000

    created ruby library via bundle gem

---
+++ lib/trains/coding/challenge.rb
@@ -0,0 +1,10 @@
+require "trains/coding/challenge/version"
+
+module Trains
+  module Coding
+    module Challenge
+      class Error < StandardError; end
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/trains/coding/challenge/version.rb
@@ -0,0 +1,7 @@
+module Trains
+  module Coding
+    module Challenge
+      VERSION = "0.1.0"
+    end
+  end
+end
