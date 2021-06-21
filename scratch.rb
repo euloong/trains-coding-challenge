5d80f601204e1ebedc7a76f7dade1e834f5a8f5f
Eu-Loong Lim <euloong@hotmail.com>
2021-06-21 12:03:03 +1000

    reworked code to implement algorithm correctly

--- scratch.rb
+++ scratch.rb
@@ -1,3 +1,4 @@
+#Ruby v2.6.3
 # Graph: AB5, BC4, CD8, DC8, DE6, AD5, CE2, EB3, AE7
 #      A   B   C   D   E
 # A    0   5   0   5   7
@@ -17,43 +18,66 @@ adjacency_matrix = [
 @infinity = 100
 visited_nodes = []
 unvisited_nodes = ['A', 'B', 'C', 'D', 'E']
-known_distance_from_A_array = [0, @infinity, @infinity, @infinity, @infinity]
-puts "Initial state is: #{known_distance_from_A_array}"
+shortest_distance_from_A = [0, @infinity, @infinity, @infinity, @infinity]
+known_distance_from_A = adjacency_matrix[0]
+
+#Setup array of arrays to track closest neighbours to A
+nodes = ['A', 'B', 'C', 'D', 'E']
+hash = Hash[(0..known_distance_from_A.size-1).zip known_distance_from_A]
+puts "hash: #{hash}"
+sorted_hash = hash.sort_by {|key, value| value}
+puts "sorted hash: #{sorted_hash}"
+immediate_neighbours_to_A_array = sorted_hash.delete_if { |k, v| (v == 0) }
+puts "array of A's immediate neighbours: #{immediate_neighbours_to_A_array}"
+puts "-------------------------------------------------------"
 #Start at A, which is index 0 in the adjacency_matrix
 start_node_index = 0
+#A is 0 from A
+distance_of_node_from_A = 0
+puts "Distance of node from A: #{distance_of_node_from_A}"
 
-#while unvisited_nodes.size > 0
-    start_node_value = unvisited_nodes.first
-    puts "Start node is: #{start_node_value}"
+start_node_value = unvisited_nodes.first
+puts "Start node is: #{start_node_value}"
+puts "-------------------------------------------------------"
+while unvisited_nodes.size > 0
     visited_status_of_start_node = visited_nodes.include?(start_node_index)
-    if (visited_status_of_start_node == false)
-        calculated_distances_from_start_node_array = adjacency_matrix[start_node_index]
-        puts "Calculated distances of neighbouring nodes from start node: #{calculated_distances_from_start_node_array}"
-#find index of smallest value in array
-        b = calculated_distances_from_start_node_array.sort
-        index_of_closest_neigbour = calculated_distances_from_start_node_array.index(b.first)
-        calculated_distances_from_closest_neighbour_node_array = adjacency_matrix[index_of_closest_neigbour]
-
-        calculated_distances_from_closest_neighbour_node_array.each_with_index do |calculated_distance, index|
-            puts "#{calculated_distance}"
-            if ((calculated_distance < known_distance_from_A_array[index])  && calculated_distance > 0)
-            known_distance_from_A_array[index] = calculated_distance
-            puts "Known dist #{known_distance_from_A_array}"
-            #end
-               #  if calculated_distance_from_A[index] < known_distance_from_A[index]
-                    # known_distance_from_A[index] = calculated_distance_from_A[index].zip(known_distance_from_A[index]).map { |x,y| x + y }
-               # known_distance_from_A[index] = calculated_distance_from_A[index] + known_distance_from_A[index]
-               #  end
+    if (immediate_neighbours_to_A_array.size > 0)
+        known_distances_from_start_node = adjacency_matrix[start_node_index]
+        puts "Known distances from start node: #{known_distances_from_start_node}"
+        calculated_distances_from_A = known_distances_from_start_node.each_with_index do |distance, index|
+            if  (distance > 0)
+                known_distances_from_start_node[index] = distance + distance_of_node_from_A
             end
         end
-        puts "New state is: #{known_distance_from_A_array}"
-    end
-    visited_nodes << start_node_value
-    unvisited_nodes = unvisited_nodes.drop(1)
-    puts "visited nodes: #{visited_nodes}"
-    puts "new unvisited nodes: #{unvisited_nodes}"
-    new_start_node = unvisited_nodes.first
-    puts "new start node: #{new_start_node}"
-    start_node_index += 1
-    puts "start node index: #{start_node_index}"
-#end
+        puts "Calculated distances from A: #{calculated_distances_from_A}"
+        calculated_distances_from_A.each_with_index do |calculated_distance, index|
+            puts "calc dist:#{calculated_distance}"
+            if ((calculated_distance < shortest_distance_from_A[index])  && calculated_distance > 0)
+                shortest_distance_from_A[index] = calculated_distance
+            puts "Temporary shortest dist from A #{shortest_distance_from_A}"
+            end
+        end
+        puts "Shortest distance from A is now: #{shortest_distance_from_A}"
+        #find index of smallest value in neighbour array 
+        index_of_first_closest_neighbour = immediate_neighbours_to_A_array[0][0]
+        puts "index of first neighbour: #{index_of_first_closest_neighbour}"
+        distance_of_first_closest_neighbour = immediate_neighbours_to_A_array[0][1]
+        puts "distance of first neighbour: #{distance_of_first_closest_neighbour}"
+        node_of_closest_neighbour = nodes[index_of_first_closest_neighbour]
+        puts "node of closest neighbour: #{node_of_closest_neighbour}"
+
+        if (immediate_neighbours_to_A_array.size > 0 && (node_of_closest_neighbour == start_node_value))
+            start_node_index = index_of_first_closest_neighbour
+            distance_of_node_from_A = distance_of_first_closest_neighbour
+            start_node_value = node_of_closest_neighbour
+            immediate_neighbours_to_A_array = immediate_neighbours_to_A_array.drop(1)
+            puts "new immediate_neighbours_to_A_array: #{immediate_neighbours_to_A_array}"
+        end
+    end 
+        visited_nodes << start_node_value
+        unvisited_nodes = unvisited_nodes.drop(1)
+        puts "visited nodes: #{visited_nodes}"
+        puts "new unvisited nodes: #{unvisited_nodes}"
+        start_node_value = unvisited_nodes.first
+end
+puts "Shortest distance from A to C is #{shortest_distance_from_A[2]}"
